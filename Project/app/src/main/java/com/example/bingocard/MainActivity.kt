cbd871112de66d2fa3145bb6d25f2531d9bb73f1
川田広輝 <f13bc005@gmail.com>
2021-11-23 11:56:21 +0900

    ビンゴカードアプリ作成  - 仮レイアウト作成  - DataBinding環境構築  - ViewModelクラス作成

--- Project/app/src/main/java/com/example/bingocard/MainActivity.kt
+++ Project/app/src/main/java/com/example/bingocard/MainActivity.kt
@@ -3,7 +3,12 @@ package com.example.bingocard
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * ビンゴカード画面_Activity
+ */
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ Project/app/src/main/java/com/example/bingocard/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.bingocard
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * ビンゴカード画面_ViewModel
+ */
+class MainViewModel : ViewModel() {
+}
